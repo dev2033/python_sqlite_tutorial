769cb5e1ca411f427e5cd148db7b1e5049029ffd
dev2033 <dev.elop.02@mail.ru>
2020-11-05 20:50:18 +0300

    pulling

---
+++ practice/pulling_data.py
@@ -0,0 +1,22 @@
+import sqlite3 as lite
+import sys
+
+
+con = lite.connect('orders.db')
+
+with con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users")
+    rows = cur.fetchall()
+    print(rows)
+
+
+with con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users")
+
+    while True:
+        row = cur.fetchone()
+        if row == None:
+            break
+        print(row[0], row[1], row[2])
